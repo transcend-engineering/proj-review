c9c379c6fbc3a134fe3ab7dd2d6c086492ce7f12
Brian Tang <byron123t@gmail.com>
2019-03-04 20:22:18 -0600

    Updated and bug fixed project review script Sprint 2019

---
+++ convert_csv.py
@@ -0,0 +1,7 @@
+import csv, os
+
+def main:
+	path = os.getcwd()
+
+if __name__ == '__main__':
+	main()
--- project_review.py
+++ project_review.py
@@ -1,19 +1,24 @@
-import os
+import os, random, signal
+from copy import deepcopy
 from csv import DictReader
 from operator import itemgetter
-import random
-from copy import deepcopy
 
-				# Want to fill in fewest students in certain time first
-				# Want to use mentors with more times first
-				# Want to use mentors with fewer students first
-				# Want to ideally use counts of areas as well
-					# Want to fill in fewest students 
-				# Option 1: extensive search with a score rubric//
-					# first search on students and mentors and their areas
-						# second search on all possible times
-				# Option 2: greedy search with many factors
-				# Option 3: random search until all assigned or for highest score
+
+# Step 1: Edit MentorSignup.csv and StudentSignup.csv which should both be downloaded and renamed from the project review signup forms. Edit the column names
+# to be 'name', 'times', 'areas', 'email'. These are the only columns that need to be extracted for this algorithm.
+# Step 2: Move results and directories as you find fit.
+# Step 3: Run python3 project_review.py
+# Step 4: Ctrl + c when you think there are enough results generated.
+# Step 5: Copy paste results to the spreadsheet created on the drive.
+
+
+boolean = True
+
+
+def signal_handler(sig, frame):
+	global boolean
+	print('\nRandom Matching Algorithm ending...')
+	boolean = False
 
 
 def loadMentors(path):
@@ -21,12 +26,13 @@ def loadMentors(path):
 	with open(path + '/MentorSignup.csv') as infile:
 		read = DictReader(infile)
 		for row in read:
-			person = {'name':'', 'times':[], 'areas':[], 'tcount':0, 'acount':0, 'count':0}
+			person = {'name':'', 'times':[], 'areas':[], 'tcount':0, 'acount':0, 'count':0, 'email':''}
 			person['name'] = row['name']
 			person['times'] = row['times'].split(', ')
 			person['tcount'] = len(person['times'])
 			person['areas'] = row['areas'].split(', ')
 			person['acount'] = len(person['areas'])
+			person['email'] = row['email']
 			mentors.append(person)
 	return mentors
 
@@ -36,12 +42,13 @@ def loadStudents(path):
 	with open(path + '/StudentSignup.csv') as infile:
 		read = DictReader(infile)
 		for row in read:
-			person = {'name':'', 'times':[], 'areas':[], 'tcount':0, 'acount':0, 'mentor':'None'}
+			person = {'name':'', 'times':[], 'areas':[], 'tcount':0, 'acount':0, 'mentor':'None', 'email':'', 'mentoremail':'None'}
 			person['name'] = row['name']
 			person['times'] = row['times'].split(', ')
 			person['tcount'] = len(person['times'])
 			person['areas'] = row['areas'].split(', ')
 			person['acount'] = len(person['areas'])
+			person['email'] = row['email']
 			students.append(person)
 	return students
 
@@ -51,7 +58,7 @@ def matchMake(path, students, mentors):
 	# timeSupply = {'6:30 - 7:00 pm': 0, '7:00 - 7:30 pm': 0, '7:30 - 8:00 pm': 0}
 	oldMentors = deepcopy(mentors)
 	oldStudents = deepcopy(students)
-	while True:
+	while boolean:
 		score = 0
 		students = deepcopy(oldStudents)
 		random.shuffle(students)
@@ -63,26 +70,38 @@ def matchMake(path, students, mentors):
 			# mentors = sorted(mentors, key=itemgetter('count', 'tcount', 'acount'))
 			random.shuffle(mentors)
 			for m in mentors:
-				if m['count'] < 2:
+				if m['count'] <= 2:
 					found = verifyMatch(s, m)
 					if found > 0:
 						score += found
 						break
 		output = True
+
 		for s in students:
 			if s['mentor'] == 'None':
 				output = False
-		if output:
-			with open(path + '/results/results' + str(score) + '.txt', 'w') as outfile:
+
+		outfilename = str(score) + '.txt'
+		if (output) & (not os.path.isfile(path + '/results/results' + outfilename)):
+			print('Matching Found: results' + str(score) + '.txt')
+			with open(path + '/results/results' + outfilename, 'w') as outfile:
 				outfile.write(str(score))
-				outfile.write('\n---\n')
+				outfile.write('\n-----\n')
 				for s in students:
-					outfile.write(s['name'] + ': ' + s['mentor'] + ': ' + s['time'])
+					outfile.write('{:25} | {:14} | {:25} | {:40} | {:100}'.format(s['mentor'], s['time'], s['name'], s['mentoremail'], s['email']))
 					outfile.write('\n')
+				outfile.write('Counts\n-----\n')
 				for m in mentors:
-					outfile.write(m['name'] + ': ' + str(m['count']))
+					outfile.write('{:25} | {:1}'.format(m['name'], m['count']))
 					outfile.write('\n')
 				outfile.write('\n')
+		else:
+			print('Something went wrong. Matching not found. Most likely there are not enough mentor time slots for the students.')
+			with open(path + '/results/results' + outfilename, 'w') as outfile:
+				for s in students:
+					if s['mentor'] == 'None':
+						outfile.write('{:25} | {:14} | {:25} | {:40} | {:100}'.format(s['mentor'], 'No Time Avail ', s['name'], s['mentoremail'], s['email']))
+	return
 
 
 def verifyMatch(student, mentor):
@@ -99,18 +118,21 @@ def verifyMatch(student, mentor):
 		random.shuffle(matches)
 		mentor['times'].remove(matches[0])
 	if first:
+		score += 1
 		for a in student['areas']:
 			if a in mentor['areas']:
 				score += 1
-				found = True
+		found = True
 	if found:
 		student['mentor'] = mentor['name']
+		student['mentoremail'] = mentor['email']
 		student['time'] = matches[0]
 		mentor['count'] += 1
 	return score
 
 
 def main():
+	signal.signal(signal.SIGINT, signal_handler)
 	path = os.getcwd()
 	mentors = loadMentors(path)
 	students = loadStudents(path)
